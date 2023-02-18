12f1ea13b974c62dfc3136a17aa062d02054d62f
dandavies23 <dan@dandavies23.com>
2023-02-18 11:22:56 +0000

    Initial commit

---
+++ run.py
@@ -0,0 +1,36 @@
+import csv
+import xml.etree.ElementTree as ET
+
+# Replace the file name with your own XML export file
+tree = ET.parse('xml-backups/track23.wordpress.2023-02-17.xml')
+root = tree.getroot()
+
+# Open a new CSV file for writing
+with open('blog-posts.csv', mode='w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+
+    # Write the header row
+    writer.writerow(['Title', 'Publication Date', 'Author', 'Body', 'Tags', 'Featured Image'])
+
+    # Loop through each post in the XML file
+    for item in root.findall('./channel/item'):
+
+        # Check if the post is a blog post
+        post_type = item.find('wp:post_type', {'wp': 'http://wordpress.org/export/1.2/'}).text
+        if post_type == 'post':
+
+            # Extract the relevant fields from the post
+            title = item.find('title').text
+            pub_date = item.find('pubDate').text
+            author = item.find('dc:creator', {'dc': 'http://purl.org/dc/elements/1.1/'}).text
+            body = item.find('content:encoded', {'content': 'http://purl.org/rss/1.0/modules/content/'}).text
+            tags = [tag.text for tag in item.findall('category')]
+                       
+            # Check if the post has a featured image and extract the URL
+            if item.find('wp:attachment_url', {'wp': 'http://wordpress.org/export/1.2/'}):
+                featured_image = item.find('wp:attachment_url', {'wp': 'http://wordpress.org/export/1.2/'}).text
+            else:
+                featured_image = ""
+
+            # Write the data to a new row in the CSV file
+            writer.writerow([title, pub_date, author, body, ', '.join(tags), featured_image])
